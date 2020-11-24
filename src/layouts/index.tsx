0622d6300e3e9b034009a9ee285f249ac23fcba1
hyroky867 <handm755@gmail.com>
2020-11-24 21:46:39 +0900

    fix: useStaticQuery

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,34 +1,30 @@
 import React from 'react';
-import { graphql, Link, StaticQuery } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import { LayoutsQuery } from '../../types/graphql-types';
 
 type Props = {
   children: React.ReactNode;
 };
 
-export const query = graphql`
-  query Layouts {
-    site {
-      siteMetadata {
-        title
+const Component: React.FC<Props> = ({ children }: Props) => {
+  const data: LayoutsQuery = useStaticQuery(graphql`
+    query Layouts {
+      site {
+        siteMetadata {
+          title
+        }
       }
     }
-  }
-`;
-
-const Component: React.FC<Props> = ({ children }: Props) => (
-  <StaticQuery
-    query={query}
-    render={(data: LayoutsQuery): React.ReactNode => (
-      <div>
-        <Link to="/">
-          <h3>{data.site?.siteMetadata?.title} Layout</h3>
-        </Link>
-        <Link to="/about">About</Link>
-        {children}
-      </div>
-    )}
-  />
-);
+  `);
+  return (
+    <>
+      <Link to="/">
+        <h3>{data.site?.siteMetadata?.title} Layout</h3>
+      </Link>
+      <Link to="/about">About</Link>
+      {children}
+    </>
+  );
+};
 
 export default Component;
