669b9e840a3863ceba6fd5288e0cccc484b01a30
hyroky867 <handm755@gmail.com>
2020-11-30 22:00:15 +0900

    feat: apply env var

--- config/site.js
+++ config/site.js
@@ -2,18 +2,18 @@ const url = `https://${process.env.DOMAIN}`
 
 module.exports = {
   pathPrefix: '/',
-  title: 'My Wonderful Website', // タイトル
-  titleAlt: 'My Wonderful Website', // JSONLDのためのタイトル
-  description: 'Welcome to my brilliant website.',
+  title: process.env.SITE_TITLE, // タイトル
+  titleAlt: process.env.SITE_TITLE, // JSONLDのためのタイトル
+  description: process.env.DESCRIPTION,
   url: url, // スラッシュなしのサイトURL
   siteUrl: `${url}/`, // スラッシュありのサイトURL
   siteLanguage: 'ja', // HTMLの言語（ここでは日本語）
   logo: '',
   banner: '',
   favicon: 'static/images/favicon.png', // ファビコン
-  shortName: 'CoolSite', // サイトの略称、12文字以下
-  author: 'so99ynoodles', // schemaORGJSONLDの作成者
+  shortName: process.env.SHORT_NAME, // サイトの略称、12文字以下
+  author: process.env.AUTHOR,
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
-  twitter: '@so996ynoodles', // TwitterのID
+  twitter: `@${process.env.TWITTER_ID}`, // TwitterのID
 };
