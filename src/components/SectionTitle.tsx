634b974f5b4e83d4bfd07f75cf2044064e638b9f
hyroky867 <handm755@gmail.com>
2021-01-08 22:05:48 +0900

    style: add index page text color

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -7,16 +7,17 @@ type Props = {
 };
 
 export const SectionTitle: React.FC<Props> = ({ content, alignCenter }: Props) => (
-  <H2 alignCenter={alignCenter}>{content}</H2>
+  <H1 alignCenter={alignCenter}>{content}</H1>
 );
 
 type H2Props = { alignCenter: boolean };
 
-const H2 = styled.h2`
+const H1 = styled.h1`
   margin: 0;
   padding: 0 0 8px 0;
   text-align: ${({ alignCenter }: H2Props): string => (alignCenter ? 'center' : 'left')};
   font-size: 24px;
+  color: var(--title-color);
   &:hover {
     cursor: pointer;
     text-decoration: underline;
